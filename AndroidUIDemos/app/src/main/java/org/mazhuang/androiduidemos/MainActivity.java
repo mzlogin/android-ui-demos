7e75e36f2565bd0cc566bec133084a47b66938dc
Zhuang Ma <chumpma@gmail.com>
2016-04-19 11:58:31 +0800

    Refactor

--- AndroidUIDemos/app/src/main/java/org/mazhuang/androiduidemos/MainActivity.java
+++ AndroidUIDemos/app/src/main/java/org/mazhuang/androiduidemos/MainActivity.java
@@ -1,126 +1,26 @@
 package org.mazhuang.androiduidemos;
 
-import android.app.Activity;
-import android.app.ExpandableListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
 
-public class MainActivity extends ExpandableListActivity {
+import org.mazhuang.androiduidemos.adapter.MyExpandableListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyExpandableListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setListAdapter(new MyExpandableListAdapter());
-    }
-
-    class MyExpandableListAdapter extends BaseExpandableListAdapter {
-        private String[] mGroups = {
-            "Fragments"
-        };
-
-        private ListItem[][] mChildren = {
-            {
-                new ListItem("update ui after popBackStack", MainActivity.class)
-            }
-        };
-
-        @Override
-        public int getGroupCount() {
-            return mGroups.length;
-        }
-
-        @Override
-        public int getChildrenCount(int groupPosition) {
-            return mChildren[groupPosition].length;
-        }
-
-        @Override
-        public Object getGroup(int groupPosition) {
-            return mGroups[groupPosition];
-        }
-
-        @Override
-        public Object getChild(int groupPosition, int childPosition) {
-            return mChildren[groupPosition][childPosition];
-        }
-
-        @Override
-        public long getGroupId(int groupPosition) {
-            return 0;
-        }
-
-        @Override
-        public long getChildId(int groupPosition, int childPosition) {
-            return 0;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return false;
-        }
-
-        @Override
-        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-            GroupViewHolder holder;
-            if (convertView == null) {
-                convertView = LayoutInflater.from(MainActivity.this)
-                        .inflate(R.layout.group_index, null);
-                holder = new GroupViewHolder();
-                holder.mGroupName = (TextView) convertView.findViewById(R.id.groupName);
-                convertView.setTag(holder);
-            } else {
-                holder = (GroupViewHolder) convertView.getTag();
-            }
-            String groupName = mGroups[groupPosition];
-            holder.mGroupName.setText(groupName);
-
-            return convertView;
-        }
-
-        @Override
-        public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-            ChildViewHolder holder;
-            if (convertView == null) {
-                convertView = LayoutInflater.from(MainActivity.this)
-                        .inflate(R.layout.item_index, null);
-                holder = new ChildViewHolder();
-                holder.mChildName = (TextView) convertView.findViewById(R.id.childName);
-                convertView.setTag(holder);
-            } else {
-                holder = (ChildViewHolder) convertView.getTag();
-            }
-            String childName = mChildren[groupPosition][childPosition].mText;
-            holder.mChildName.setText(childName);
-
-            return convertView;
-        }
-
-        @Override
-        public boolean isChildSelectable(int groupPosition, int childPosition) {
-            return false;
+        mAdapter = new MyExpandableListAdapter(this);
+        ExpandableListView listView = (ExpandableListView) findViewById(R.id.list);
+        if (listView != null) {
+            listView.setAdapter(mAdapter);
+            listView.setOnChildClickListener(mAdapter);
         }
     }
 
-    class ListItem {
-        private String mText;
-        private Class<? extends Activity> mActivity;
-        public ListItem(String text, Class<? extends Activity> activity) {
-            mText = text;
-            mActivity = activity;
-        }
-    }
-
-    class GroupViewHolder {
-        public TextView mGroupName;
-    }
-
-    class ChildViewHolder {
-        public TextView mChildName;
-    }
 }
---
+++ AndroidUIDemos/app/src/main/java/org/mazhuang/androiduidemos/adapter/MyExpandableListAdapter.java
@@ -0,0 +1,142 @@
+package org.mazhuang.androiduidemos.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import org.mazhuang.androiduidemos.R;
+import org.mazhuang.androiduidemos.activity.FragmentUpdateUIActivity;
+import org.mazhuang.androiduidemos.activity.NotificationDemoActivity;
+
+/**
+ * Created by mazhuang on 4/19.
+ */
+public class MyExpandableListAdapter extends BaseExpandableListAdapter implements ExpandableListView.OnChildClickListener {
+    private Context mContext;
+
+    private String[] mGroups = {
+            "Fragments",
+            "Notifications"
+    };
+
+    private ListItem[][] mChildren = {
+            { // Fragments
+                    new ListItem("update ui after popBackStack", FragmentUpdateUIActivity.class)
+            },
+            { // Notifications
+                    new ListItem("test Notifications", NotificationDemoActivity.class)
+            }
+    };
+
+    public MyExpandableListAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return mGroups.length;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return mChildren[groupPosition].length;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return mGroups[groupPosition];
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return mChildren[groupPosition][childPosition];
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        GroupViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.group_index, null);
+            holder = new GroupViewHolder();
+            holder.mGroupName = (TextView) convertView.findViewById(R.id.groupName);
+            convertView.setTag(holder);
+        } else {
+            holder = (GroupViewHolder) convertView.getTag();
+        }
+        String groupName = mGroups[groupPosition];
+        holder.mGroupName.setText(groupName);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ChildViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.item_index, null);
+            holder = new ChildViewHolder();
+            holder.mChildName = (TextView) convertView.findViewById(R.id.childName);
+            convertView.setTag(holder);
+        } else {
+            holder = (ChildViewHolder) convertView.getTag();
+        }
+        String childName = mChildren[groupPosition][childPosition].mText;
+        holder.mChildName.setText(childName);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    @Override
+    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+        ListItem item = mChildren[groupPosition][childPosition];
+        Intent intent = new Intent(mContext, item.mActivityClass);
+        mContext.startActivity(intent);
+        return true;
+    }
+
+    class ListItem {
+        private String mText;
+        private Class<? extends Activity> mActivityClass;
+
+        public ListItem(String text, Class<? extends Activity> activity) {
+            mText = text;
+            mActivityClass = activity;
+        }
+    }
+
+    class GroupViewHolder {
+        public TextView mGroupName;
+    }
+
+    class ChildViewHolder {
+        public TextView mChildName;
+    }
+}
