426495e9d11affa2b99a213a602fd25b927bd3d9
Zhuang Ma <chumpma@gmail.com>
2016-04-19 20:39:21 +0800

    Add custom layout notification

--- AndroidUIDemos/app/src/main/java/org/mazhuang/androiduidemos/activity/NotificationDemoActivity.java
+++ AndroidUIDemos/app/src/main/java/org/mazhuang/androiduidemos/activity/NotificationDemoActivity.java
@@ -1,15 +1,17 @@
 package org.mazhuang.androiduidemos.activity;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.support.v4.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RemoteViews;
 
 import org.mazhuang.androiduidemos.MainActivity;
 import org.mazhuang.androiduidemos.R;
@@ -17,6 +19,7 @@ import org.mazhuang.androiduidemos.R;
 public class NotificationDemoActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int SIMPLE_NOTIFICATION_ID = 0;
+    private static final int CUSTOM_LAYOUT_NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,11 @@ public class NotificationDemoActivity extends AppCompatActivity implements View.
         if (simpleNotificationBtn != null) {
             simpleNotificationBtn.setOnClickListener(this);
         }
+
+        Button customLayoutNotificationBtn = (Button)findViewById(R.id.customLayoutNotification);
+        if (customLayoutNotificationBtn != null) {
+            customLayoutNotificationBtn.setOnClickListener(this);
+        }
     }
 
     @Override
@@ -35,7 +43,50 @@ public class NotificationDemoActivity extends AppCompatActivity implements View.
             case R.id.simpleNotification:
                 sendSimpleNotification();
                 break;
+            case R.id.customLayoutNotification:
+                sendCustomLayoutNotification();
+                break;
         }
+    }
+
+    private void sendCustomLayoutNotification() {
+        RemoteViews contentViews = new RemoteViews(getPackageName(), R.layout.notification_custom);
+        contentViews.setImageViewResource(R.id.bigPicture,
+                android.R.drawable.star_big_on);
+        contentViews.setTextViewText(R.id.notificationText,
+                getString(R.string.custom_layout_notification));
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.custom_layout_notification))
+                .setAutoCancel(true);
+        builder.setOngoing(true);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_CANCEL_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+        Notification notification = builder.build();
+
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = builder.build();
+            notification.bigContentView = contentViews;
+        }
+        notification.contentView = contentViews;
+        // builder.setContent(contentViews);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(CUSTOM_LAYOUT_NOTIFICATION_ID, notification);
+        // notificationManager.notify(CUSTOM_LAYOUT_NOTIFICATION_ID, builder.build());
     }
 
     private void sendSimpleNotification() {
